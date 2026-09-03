a8a2103106c5cec530ba8159a7d3091ca9786197
Jake Vichnis <149420250+jakevichnis@users.noreply.github.com>
2026-09-03 10:23:57 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        table = [0] * (n + 1)
+        table[0] = 1
+        table[1] = 1
+        for i in range(2, n + 1):
+            table[i] = table[i-1] + table[i-2]
+        return table[n]
